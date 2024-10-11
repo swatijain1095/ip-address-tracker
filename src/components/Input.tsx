d5a1752b7cc63be6b142d1d079efc0dfb057ac11
Swati Jain <jain.swati71@gmail.com>
2024-10-11 15:26:24 +0200

    added react-icon and fixed style

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,14 +2,21 @@ interface InputProps {
   className?: string;
   placeholder?: string;
   style?: React.CSSProperties;
+  type?: string;
 }
 
 export const Input = ({
   className,
   placeholder = "Search for any IP address",
   style,
+  type,
 }: InputProps) => {
   return (
-    <input className={className} placeholder={placeholder} style={style} />
+    <input
+      className={className}
+      placeholder={placeholder}
+      style={style}
+      type={type}
+    />
   );
 };
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,21 +3,29 @@ import type { Map as LeafletMap } from "leaflet";
 import { useRef } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { Input } from "./Input";
+import { GoChevronRight } from "react-icons/go";
 
 export default function Map() {
   const mapRef = useRef<LeafletMap | null>(null);
 
   return (
     <div className="relative h-screen w-screen">
-      <div className="absolute top-0 left-0 right-0 bg-customPurple p-10 z-[1000]">
+      <div className="absolute top-0 left-0 right-0 bg-customPurple p-6 z-[1000] rounded-b-2xl">
         <h1 className="text-5xl text-white font-bold text-center">
           IP Address Tracker
         </h1>
       </div>
-      <Input
-        className="absolute p-2 top-28 left-1/2 transform -translate-x-1/2 h-12 rounded-lg shadow-lg shadow-gray-200 bg-gray-50 z-[1000] focus:outline-none"
-        style={{ width: "450px" }}
-      />
+      <div className="absolute flex items-center p-4 top-20 left-1/2 transform -translate-x-1/2 h-12 rounded-lg shadow-lg shadow-gray-200 bg-gray-50 z-[1000]">
+        <Input
+          className="bg-gray-50 focus:outline-none"
+          style={{ width: "550px" }}
+          type="text"
+        />
+        <GoChevronRight size={24} color="#6C5DD4" />
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 bg-customPurple p-8 z-[1000] rounded-t-2xl">
+        <h1 className="text-5xl text-white font-bold text-center">test</h1>
+      </div>
       <MapContainer
         ref={mapRef}
         center={[40.7, -74]}
