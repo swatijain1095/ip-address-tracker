6f89b13e216c007abd6cc4ffab5d16f6136ed981
Swati Jain <jain.swati71@gmail.com>
2024-12-16 18:09:32 +0100

    updated api.ts file and mapped api output

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,12 @@
+import { Geolocation } from "./type";
+
 const apiKey = import.meta.env.VITE_API_KEY;
-type GeoLocationProps = {
+
+export const getGeoLocation = async ({
+  ip,
+}: {
   ip: string;
-};
-export const getGeoLocation = async ({ ip }: GeoLocationProps) => {
+}): Promise<Geolocation> => {
   const response = await fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`
   );
@@ -12,5 +16,31 @@ export const getGeoLocation = async ({ ip }: GeoLocationProps) => {
   }
 
   const data = await response.json();
-  return data;
+
+  const geoLocationData: Geolocation = {
+    ip: data.ip,
+    location: {
+      country: data.location.country,
+      region: data.location.region,
+      city: data.location.city,
+      lat: data.location.lat,
+      lng: data.location.lng,
+      postalCode: data.location.postalCode || "",
+      timezone: data.location.timezone,
+      geonameId: data.location.geonameId,
+    },
+    isp: data.isp,
+    domains: data.domains || [],
+    as: data.as
+      ? {
+          asn: data.as.asn,
+          name: data.as.name,
+          route: data.as.route,
+          domain: data.as.domain || "",
+          type: data.as.type || "",
+        }
+      : undefined,
+  };
+
+  return geoLocationData;
 };
