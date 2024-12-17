65e0dcf6eda1c24564cc6abc6d7d95c4a28f3c9d
Swati Jain <jain.swati71@gmail.com>
2024-12-17 16:53:10 +0100

    updated api file

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,6 @@
-import { Geolocation } from "./type";
-
 const apiKey = import.meta.env.VITE_API_KEY;
 
-export const getGeoLocation = async ({
-  ip,
-}: {
-  ip: string;
-}): Promise<Geolocation> => {
+export const getGeoLocation = async (ip?: string) => {
   const response = await fetch(
     `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`
   );
@@ -16,31 +10,5 @@ export const getGeoLocation = async ({
   }
 
   const data = await response.json();
-
-  const geoLocationData: Geolocation = {
-    ip: data.ip,
-    location: {
-      country: data.location.country,
-      region: data.location.region,
-      city: data.location.city,
-      lat: data.location.lat,
-      lng: data.location.lng,
-      postalCode: data.location.postalCode || "",
-      timezone: data.location.timezone,
-      geonameId: data.location.geonameId,
-    },
-    isp: data.isp,
-    domains: data.domains || [],
-    as: data.as
-      ? {
-          asn: data.as.asn,
-          name: data.as.name,
-          route: data.as.route,
-          domain: data.as.domain || "",
-          type: data.as.type || "",
-        }
-      : undefined,
-  };
-
-  return geoLocationData;
+  return data;
 };
